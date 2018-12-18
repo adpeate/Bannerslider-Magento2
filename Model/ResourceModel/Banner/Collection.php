08c9d68510f6855874da87fd2317380c31b34f0f
Alan Peate <alan@magerage.co.uk>
2018-12-18 10:43:34 +0000

    removed fixed width css. Fixed banner collection resource model

--- Model/ResourceModel/Banner/Collection.php
+++ Model/ResourceModel/Banner/Collection.php
@@ -87,10 +87,10 @@ class Collection extends \Magento\Framework\Model\ResourceModel\Db\Collection\Ab
         \Magento\Framework\Data\Collection\Db\FetchStrategyInterface $fetchStrategy,
         \Magento\Framework\Event\ManagerInterface $eventManager,
         \Magento\Store\Model\StoreManagerInterface $storeManager,
-        \Magento\Framework\DB\Adapter\AdapterInterface $connection = null,
-        \Magento\Framework\Model\ResourceModel\Db\AbstractDb $resource = null
+        \Magento\Framework\Stdlib\DateTime\Timezone $stdTimezone,
+        \Magestore\Bannerslider\Model\Slider $slider
     ) {
-        parent::__construct($entityFactory, $logger, $fetchStrategy, $eventManager, $connection, $resource);
+        parent::__construct($entityFactory, $logger, $fetchStrategy, $eventManager, null, null);
         $this->_storeManager = $storeManager;
         $this->_stdTimezone = $stdTimezone;
         $this->_slider = $slider;
